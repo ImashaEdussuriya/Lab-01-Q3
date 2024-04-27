5a1909048d54c1bd96c055c01c4b10a2e5332ad8
Imasha <Imasha@SILVERBUNNY>
2024-04-27 23:27:51 +0530

    Add project files.

---
+++ Lab 01 Q3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a positive integer:");
+            int number;
+
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                if (number > 0)
+                {
+                    int sum = 0;
+                    for (int i = 1; i <= number; i++)
+                    {
+                        sum += i;
+                    }
+                    Console.WriteLine($"The sum of all numbers from 1 to {number} is: {sum}");
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+    }
+}
